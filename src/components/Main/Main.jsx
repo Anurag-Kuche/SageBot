a4faebbb29671477f795411de315777273d09e52
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-14 15:11:09 +0530

    loader updated

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -35,6 +35,18 @@ const Main = () => {
             <img src={assets.user_icon} alt="" />
             <p>{recentPrompt}</p>
           </div>
+          <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            {loading
+            ?<div className='loader'>
+                <hr />
+                <hr />
+                <hr />
+            </div>
+            : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+            }
+           
+          </div>
         </div>
         }
         <div className="greet">

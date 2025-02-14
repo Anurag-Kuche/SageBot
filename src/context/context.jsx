736cd00c7220d47ff32db6dbc691d35e1b30dfe3
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-14 16:02:21 +0530

    writing font styled

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -11,6 +11,10 @@ const ContextProvider=(props)=>{
     const[loading,setLoading]=useState(false);
     const[resultData,setResultData]=useState("");
 
+    const delayPara=(index,nextWord)=>{
+
+    }
+
     const onSent = async(prompt)=>{
         setResultData("")
         setLoading(true)
@@ -18,7 +22,18 @@ const ContextProvider=(props)=>{
         setRecentPrompt(input)
 
         const response=await run(input);
-        setResultData(response);
+        let responseArray=response.split("**");
+        let newResponse;
+        for(let i=0;i<responseArray.length;i++){
+            if(i===0 || i%2!==1){
+                newResponse+=responseArray[i];
+            }else{
+                newResponse+="<b>"+responseArray[i]+"</b>"
+            }
+        }
+        let newResponse2=newResponse.split("*").join("</br>")
+
+        setResultData(newResponse2);
         setLoading(false);
         setInput("")
 
